c0eb4e3d0198e5ba29ac125ed662815eafc39125
Sean M <43443470+greydoubt@users.noreply.github.com>
2023-05-11 23:39:43 -0700

    Add files via upload

---
+++ langchain_python_toolkit.py
@@ -0,0 +1,12 @@
+from langchain.agents.agent_toolkits import create_python_agent
+from langchain.tools.python.tool import PythonREPLTool
+from langchain.python import PythonREPL
+from langchain.llms.openai import OpenAI
+
+agent_executor = create_python_agent(
+    llm=OpenAI(temperature=0, max_tokens=1000),
+    tool=PythonREPLTool(),
+    verbose=True
+)
+
+agent_executor.run("""This generates demo code that will be sent into a python repl, executed, and its output returned. What is the ramsey number for a 5-colour clique of 30?  """)
